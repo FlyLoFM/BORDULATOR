9c53df9fa082532be2b1a49346a0db779ab44094
noodleWrecker7 <hodgkinson.adam@gmail.com>
2018-09-07 18:57:28 +0100

    yeah um theres stuff

---
+++ src/Operations.java
@@ -0,0 +1,11 @@
+public class Operations {
+
+    public static int add(int num1, int num2) {
+        int out;
+
+        out = num1 + num2;
+
+        return out;
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,3 @@
+public class main {
+    // i dont know if we need this
+}
